93024b4123031d2dde4e985219b425b8d0b5b96a
ahmed0-17 <malikalihyd123@gmail.com>
2026-09-01 23:58:35 +0500

    Week3 Day1

--- 01_Python/Week_3/day_1/pathlib.py
+++ 01_Python/Week_3/day_1/pathlib.py
@@ -4,3 +4,18 @@ documents = Path("data/documents")
 
 for file in documents.glob("*.txt"):
     print(file)
+
+
+
+
+
+
+path = Path("documents")
+
+if path.exists():
+    if path.is_dir():
+        print("This is a folder")
+    elif path.is_file():
+        print("This is a file")
+else:
+    print("Path does not exist")    

8bd59e5eaf751218530c4a8aa06d9eb0468e35ab
ahmed0-17 <malikalihyd123@gmail.com>
2026-09-19 01:53:20 +0500

    Week4 Day4

--- 01_Python/Week_4/Day_3/async_api_collector.py
+++ 01_Python/Week_4/Day_3/async_api_collector.py
@@ -1,7 +1,8 @@
 import asyncio
 import httpx
 import logging
-
+import json
+from pathlib import Path
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,28 +14,20 @@ logger = logging.getLogger(__name__)
 
 
 async def get_data():
+    urls = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/todos"
+        ]
     try:
+
         logger.info("Start fetching data")
 
         async with httpx.AsyncClient() as client:
 
-            users_task = asyncio.create_task(
-                client.get("https://jsonplaceholder.typicode.com/users")
-            )
+            tasks=[asyncio.create_task(client.get(url))   for url in urls]
 
-            posts_task = asyncio.create_task(
-                client.get("https://jsonplaceholder.typicode.com/posts")
-            )
-
-            todos_task = asyncio.create_task(
-                client.get("https://jsonplaceholder.typicode.com/todos")
-            )
-
-            users, posts, todos = await asyncio.gather(
-                users_task,
-                posts_task,
-                todos_task
-            )
+            users, posts, todos = await asyncio.gather(*tasks)
 
             users.raise_for_status()
             posts.raise_for_status()
@@ -47,10 +40,10 @@ async def get_data():
             logger.info("Data fetched successfully")
 
             return {
-                "users": result1,
-                "posts": result2,
-                "todos": result3
-            }
+              "users":result1,
+              "posts":result2,
+               "todos":result3
+          }
 
     except httpx.RequestError as error:
         logger.error(f"Request Error: {error}")
@@ -58,20 +51,53 @@ async def get_data():
     except httpx.HTTPStatusError as error:
         logger.error(f"Status Error: {error}")
 
+def process_data(users,posts,todos):
+   logger.info("Processing users data")
+   processed_users=[]
+   for user in users:
+          if "id" not in user:
+             logger.error("User data missing 'id'")
+             continue
+          user_id=user["id"]
+
+          user_posts=[post for post in posts if user_id==post["userId"]]
+          user_todos=[todo for todo in todos if user_id==todo["userId"]]
+
+  
+          processed_users.append({
+              "id":user_id,
+              "name":user["name"],
+              "email":user["email"],
+              "total_posts":len(user_posts),
+              "total_todos":len(user_todos)
+
+          })
+   logger.info("Users data processed successfully")
+   return processed_users
+
+
+def save_data(data_dir,processed_data):
+     data_dir.mkdir(exist_ok=True,parents=True)
+     file_path= data_dir / "data.json"   
+     with open(file_path,"w") as file:
+           json.dump(processed_data,file,indent=4)
+           logger.info(f"Saved {len(processed_data)} users")        
+    
 
 async def main():
     result = await get_data()
 
+    if result is None:
+     logger.error("Failed to fetch data")
+     return
+
     users=result["users"]
     posts=result["posts"]
     todos=result["todos"]
+    data_dir= Path("01_Python/Week_4/Day_3/Data")
 
-    for user in users:
-          user_id=user["id"]
-
-          user_posts=[post for post in posts if user_id==post["userId"]]
-
-          print(f"{user["name"]} Posts :{len(user_posts)}")  
+    processed_data=process_data(users,posts,todos)
+    save_data(data_dir,processed_data)
 
 
 asyncio.run(main())
